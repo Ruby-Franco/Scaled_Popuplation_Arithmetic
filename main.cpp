d11cdb992ffe51c01a94f91759d3ce8cfbe88cb8
Ruby-Franco <52841044+Ruby-Franco@users.noreply.github.com>
2021-08-10 15:47:36 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,18 +6,59 @@
 using namespace std; 
 
 int main(){
+	//SP-based Skewed Addition
 
-	int x; // inputs to be in binary format
-	int y; // x is σ , y is π
+	int xσ, xπ;
+	int yσ, yπ; 
+
+	cout << "input x: "; 
+	cin >> xσ >> xπ;
+
+	cout << "input y: " ; 
+	cin >> yσ >> yπ ;
+
+	string x = to_string(xπ);  // FIXME: drops beginning 0s 
+	string y = to_string(yπ);
+
+	int xpi = x.size();
+	int ypi = y.size();
+
+	string mxString, myString; 
+
+	cout << xpi << " " << ypi << endl;
+
+	for(int i = 0; i < (xpi/2);i++){
+		mxString.append("1");
+	}
+	for(int i = xpi/2; i < xpi; i++){
+		mxString.append("0");
+	}
+
+	int mx = stoi(mxString);
+	cout << "mx: " << mx << endl;
+
+	for(int i = 0; i < (ypi/2);i++){
+		myString.append("0");
+	}
+	for(int i = ypi/2; i < ypi; i++){
+		myString.append("1");
+	}
+
+	int my = stoi(myString); 	//  FIXME: drops beginning 0's
+	cout << "my: " << my << endl;
+	
+	// turning into SP-format
+	
+	int a, b; // a is σ , b is π
 
 	cout << "input first number: " ;
-	cin >> x; 
+	cin >> a; 
 
 	cout << "input second number: "; 
-	cin >> y; 
+	cin >> b; 
 	
-	// finding absPi(# of 1s in y input) and Pi(# of binary length of y input)
-	string s = to_string(y); 
+	// finding absPi(# of 1s in b) and Pi(length of b)
+	string s = to_string(b); 
 	int counter = 0; 
 
 	for(int i = 0; i < s.size(); i++){
@@ -33,8 +74,8 @@ int main(){
 	cout << "absolute value of Pi : " << absPi << endl;
 	cout << "value of pi: " << pi << endl;
 	
-	//finding lower(decimal form of x input) and upper(# of binary length of x) sigma
-	string t = to_string(x); 
+	//finding lower(decimal form of a) and upper(length of a) sigma
+	string t = to_string(a); 
 	
 	int upperSigma = t.size(); 
 	int lowerSigma = stoi(t, nullptr, 2);
